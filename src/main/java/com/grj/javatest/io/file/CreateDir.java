f205d74f215348c65a6f8a625a3160fe4fd7a1c9
grj <875654381@qq.com>
2018-04-08 18:13:42 +0800

    1

---
+++ src/main/java/com/grj/javatest/io/file/CreateDir.java
@@ -0,0 +1,13 @@
+package com.grj.javatest.io.file;
+
+import java.io.File;
+
+public class CreateDir {
+
+	public static void main(String args[]) {
+		String dirname = "src/main/java/com/grj/javatest/io/file/test";
+		File d = new File(dirname);
+		// 现在创建目录
+		d.mkdirs();
+	}
+}
