a0915359b08050fe8b7b87a71d6e0f0906542c83
grj001 <875654381@qq.com>
2018-10-11 13:28:21 +0800

    11

---
+++ src/main/java/com/java/demo/lambda/DoubleUaryOpeartorDemo.java
@@ -0,0 +1,14 @@
+package com.java.demo.lambda;
+
+import java.util.function.DoubleUnaryOperator;
+
+public class DoubleUaryOpeartorDemo{
+
+    public static void main(String[] args){
+        DoubleUnaryOperator d = x ->{
+            return x*2;
+        };
+        System.out.println(d.applyAsDouble(1.0D));
+    }
+
+}
